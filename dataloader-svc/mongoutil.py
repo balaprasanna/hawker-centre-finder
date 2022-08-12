218f7bad1d0c5c46c61c2c5c3bfa91103aac13c3
PrasannaVeerapandi <prasanna.veerapandi@traxretail.com>
2022-08-12 13:49:04 +0800

    code cleanup

--- dataloader-svc/mongoutil.py
+++ dataloader-svc/mongoutil.py
@@ -1,26 +1,26 @@
-
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
 class MongoConnector:
-	conn = None
-	srv = "mongodb+srv://{username}:{password}@{db_host}/?retryWrites=true&w=majority"
+    conn = None
+    srv = "mongodb+srv://{username}:{password}@{db_host}/?retryWrites=true&w=majority"
 
-	@classmethod
-	def connect(cls):
-		if cls.conn is None:
-			db_srv = cls.srv.format(
-				username=os.getenv("MONGO_USER", "<appuser>"), 
-				password=os.getenv("MONGO_PASSWORD", "<password>"),
-				db_host=os.getenv("MONGO_DB_HOST", "<db_host>")
-				)
-			cls.conn = MongoClient(db_srv)
-		return cls.conn
+    @classmethod
+    def connect(cls):
+        if cls.conn is None:
+            db_srv = cls.srv.format(
+                username=os.getenv("MONGO_USER", "<appuser>"),
+                password=os.getenv("MONGO_PASSWORD", "<password>"),
+                db_host=os.getenv("MONGO_DB_HOST", "<db_host>")
+            )
+            cls.conn = MongoClient(db_srv)
+        return cls.conn
 
-	@classmethod
-	def disconnect(cls):
-		cls.conn.close()
-		cls.conn = None
+    @classmethod
+    def disconnect(cls):
+        cls.conn.close()
+        cls.conn = None
